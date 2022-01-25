e8b323b4b8875aab705744f6f9913bee0d4756fd
晨风 <cezres@163.com>
2022-01-25 17:41:27 +0800

    Init project

---
+++ XCCleaner/XCCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCCleanerApp.swift
+//  XCCleaner
+//
+//  Created by azusa on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct XCCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
